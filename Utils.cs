29fe5b3770f30c916ab5fea3c19bfa4a2dcf1a46
Patrick Hannon <patrick_hannon@hotmail.com>
2019-02-27 13:59:03 -0600

    initial commit for utils

---
+++ Utils.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Web.Security;
+using System;
+using System.Text;
+
+namespace Hannon.Utils
+{
+    public static class Utils
+    {
+        public static void LogToEventLog(string source, string message)
+        {
+            using (EventLog eventLog = new EventLog(source))
+            {
+                eventLog.Source = source;
+                eventLog.WriteEntry(message, EventLogEntryType.Information, 101, 1);
+            }
+        }
+
+        private const string Purpose = "Authentication Token";
+
+        public static string Protect(string unprotectedText)
+        {
+            var unprotectedBytes = Encoding.UTF8.GetBytes(unprotectedText);
+            var protectedBytes = MachineKey.Protect(unprotectedBytes, Purpose);
+            var protectedText = Convert.ToBase64String(protectedBytes);
+            return protectedText;
+        }
+
+        public static string Unprotect(string protectedText)
+        {
+            var protectedBytes = Convert.FromBase64String(protectedText);
+            var unprotectedBytes = MachineKey.Unprotect(protectedBytes, Purpose);
+            var unprotectedText = Encoding.UTF8.GetString(unprotectedBytes);
+            return unprotectedText;
+        }
+
+    }
+}
