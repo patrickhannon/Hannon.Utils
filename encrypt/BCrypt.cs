9099c92a4d2b1d0cb3cb63ab12f91e28f71481a3
patrickhannon <patrick_hannon@hotmail.com>
2019-04-02 11:39:22 -0500

    added hashing for password using BCrypt

---
+++ encrypt/BCrypt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hannon.Utils.encrypt
+{
+    public static class BCrypt
+    {
+        public static string HashPassword(string pwd)
+        {
+            return BCrypt.HashPassword(pwd);
+        }
+
+        public static bool VerifyPassword(string submittedPwd, string hashedPwd)
+        {
+            return BCrypt.VerifyPassword(submittedPwd, hashedPwd);
+        }
+    }
+}
