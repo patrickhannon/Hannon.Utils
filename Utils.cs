6a7fd1c736f680e0696569f2a878dcbb5608599b
patrickhannon <patrick_hannon@hotmail.com>
2019-04-05 15:34:09 -0500

    added encryption stuff

--- Utils.cs
+++ Utils.cs
@@ -33,6 +33,15 @@ namespace Hannon.Utils
             var unprotectedText = Encoding.UTF8.GetString(unprotectedBytes);
             return unprotectedText;
         }
+        public static DateTime EndOfDay(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 23, 59, 59, 999);
+        }
+
+        public static DateTime StartOfDay(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, 0);
+        }
 
     }
 }
